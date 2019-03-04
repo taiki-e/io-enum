4b6d4f6ee4727ffcd0ad06cb33660ab1915b598f
Taiki Endo <te316e89@gmail.com>
2019-03-04 16:45:50 +0900

    Tweak formats and lints

--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(feature = "read_initializer", feature(read_initializer))]
 #![deny(warnings)]
+#![deny(unsafe_code)]
+#![deny(bare_trait_objects, elided_lifetimes_in_paths)]
 #![allow(dead_code)]
 
 extern crate io_enum;
