b7c7de2753bde9e73b7f0daf38e0b36a88da4825
Taiki Endo <te316e89@gmail.com>
2020-10-25 23:06:14 +0900

    Expand glob imports

--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,7 @@
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 #![allow(dead_code)]
 
-use io_enum::*;
+use io_enum::{BufRead, Read, Seek, Write};
 
 #[derive(Read, Write, Seek, BufRead)]
 enum Either<A, B> {
