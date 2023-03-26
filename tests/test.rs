809086289d3b097200a6794922ec605de4c97f6d
Taiki Endo <te316e89@gmail.com>
2023-03-26 22:21:34 +0900

    Add assertion to test

--- tests/test.rs
+++ tests/test.rs
@@ -1,10 +1,17 @@
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 #![allow(dead_code)]
 
+use std::io;
+
 use io_enum::{BufRead, Read, Seek, Write};
 
 #[derive(Read, Write, Seek, BufRead)]
 enum Either<A, B> {
     A(A),
     B(B),
+}
+
+fn _assert_impl<T: io::Read + io::Write + io::Seek + io::BufRead>() {
+    fn __assert_impl<T: io::Read + io::Write + io::Seek + io::BufRead>() {}
+    __assert_impl::<Either<T, T>>();
 }
